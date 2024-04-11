7e113bc2d836c7c7525d65129547fb4e3ab25eea
Narayan1985 <104840704+Narayan1985@users.noreply.github.com>
2024-04-11 18:12:48 -0500

    Add files via upload

--- Rice model.R
+++ Rice model.R
@@ -55,26 +55,47 @@ summary(model)
 plot(model)
 
 # Feature importance
+correlation <- cor(num_data, data$Yield)
+View(cor_matrix)
+corrplot(cor_matrix, method = "square", type = "full", 
+         order = "original", tl.col = "black", tl.srt = 45, number.font=2, pch=1, 
+         number.cex =0.5, tl.cex=0.5, mar = c(0, 0, 0, 0) )
+
 coeficientes <- coef(model)[-1]  # Exclude intercept
 importance <- abs(coeficientes)
-importance[is.na(importance)] <- 0  # Handle NA values
-importance <- importance / sum(importance)  # Normalize importance values
-importance_df <- data.frame(Feature = names(importance), Importance = importance)
-importance_df <- importance_df[order(importance_df$Importance, decreasing = TRUE), ]
-importance_df <- importance_df[!importance_df$Feature %in% c("ID", "SeasonB", "site1_Huila_Campoalegre","Site2_Huila_Agrado","Site3_Huila_Campoalegre","Site4_Huila_Villavieja","Site5_Huila_Villavieja",
-                                                             "Site6_Huila_Palermo","Site7_Huila_Campoalegre","Site8_Huila_Baraya","Site9_Huila_Neiva","Site10_Huila_Villavieja","Site11_Tolima_Ibague", 
-                                                             "Site12_Tolima_Coello","Site13_Tolima_Espinal","Site14_Tolima_Lerida","Site15_Tolima_Melgar","Site16_Tolima_Purificacion","Site17_Tolima_Prado",
-                                                             "Site18_Tolima_Guamo","Site19_Tolima_Ambalema","Site20_Tolima_Coyaima","Site21_Tolima_Flandes","Site22_Tolima_Sanluis","Site23_Tolima_Piedras",
-                                                             "Site24_Tolima_Lerida","Site25_Tolima_Armero","Site26_Tolima_Honda"), ]
+importance[is.na(importance)] <- 0  # Manage values NA
+importance <- importance / sum(importance) 
+
+# Create a vector to delete features 
+caracteristicas_a_eliminar <- c("SeasonB", "site1_Huila_Campoalegre", "Site2_Huila_Agrado", "Site3_Huila_Campoalegre", "Site4_Huila_Villavieja",
+                                "Site5_Huila_Villavieja", "Site6_Huila_Palermo", "Site7_Huila_Campoalegre", "Site8_Huila_Baraya", "Site9_Huila_Neiva",
+                                "Site10_Huila_Villavieja", "Site11_Tolima_Ibague", "Site12_Tolima_Coello", "Site13_Tolima_Espinal", "Site14_Tolima_Lerida",
+                                "Site15_Tolima_Melgar", "Site16_Tolima_Purificacion", "Site17_Tolima_Prado", "Site18_Tolima_Guamo", "Site19_Tolima_Ambalema",
+                                "Site20_Tolima_Coyaima", "Site21_Tolima_Flandes", "Site22_Tolima_Sanluis", "Site23_Tolima_Piedras", "Site24_Tolima_Lerida",
+                                "Site25_Tolima_Armero", "Site26_Tolima_Honda")
+
+# Delete the mportance vector features 
+importance <- importance[!(names(importance) %in% caracteristicas_a_eliminar)]
+
+# Normalice the importance values
+importance <- importance / sum(importance)
+
+importance_df <- data.frame(Feature = names(importance), Importance = importance, Correlation_with_Yield = correlation[-ncol(correlation)])
+
+importance_df <- importance_df[order(abs(importance_df$Correlation_with_Yield), decreasing = TRUE), ]
+
+ggplot(importance_df, aes(x = reorder(Feature, Correlation_with_Yield), y = Importance)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  labs(x = "Feature", y = "Importance") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 print(importance_df)
+
 ggplot(importance_df, aes(x = reorder(Feature, Importance), y = Importance)) +
   geom_bar(stat = "identity", fill = "skyblue") +
   labs(x = "Feature", y = "Importancia") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-
-
-
 # Step 8: Export the results to Excel
 model_output <- data.frame(Actual = test_data$Yield, Predicted = predictions)
+View(model_output)
 write.xlsx(model_output, "model_rice_output.xlsx")
